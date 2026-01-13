c2bfb462ebfe2e6257cdc7d1ca4da6e2e661ed9f
Anish Sinha <anish28april@gmail.com>
2026-01-14 00:12:32 +0530

    Implement initial FastAPI setup with folder structure  and configuration management

---
+++ core/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # App
+    APP_NAME: str = "Boomer"
+    PORT: int = 8000
+
+    # Logging
+    LOG_LEVEL: str = "INFO"
+
+    # Security
+    JWT_SECRET: str
+
+    # Database
+    DATABASE_URL: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from api.auth import router as auth_router
+from core.config import settings as Config
 
-app = FastAPI()
+app = FastAPI(title=Config.APP_NAME)
+
+app.include_router(auth_router)
 
 @app.get("/")
 def health():
